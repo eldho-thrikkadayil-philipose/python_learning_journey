9e39c13a55f9a893e165e57a31c36749f883bc6c
Eldho Philipose <eldhothrikkadayilphilipose@gmail.com>
2026-09-25 02:20:58 +0200

    Add input and output example

---
+++ ioexample.py
@@ -0,0 +1,12 @@
+a = 17
+
+print(a, 'Hello', 23, 4.5)
+
+print ('a string:')
+
+s1 = input()
+s2 = input('another string')
+i = int(input('an integer: '))
+f = float(input('a float: '))
+
+print('You entered', s1, s2, i, f)
